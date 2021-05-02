606ba936606039d6b085c7e4dbc85bf692c8f990
fahmi ahmad <fahmiahmad@tutanota.com>
2021-05-02 23:03:12 +0700

    erc20, solidity 0.8

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -25,11 +25,17 @@ async function main() {
   const Token = await hre.ethers.getContractFactory("Token");
   const token = await Token.deploy();
 
+  // ERC20 standard
+  const NDToken = await hre.ethers.getContractFactory("NDToken");
+  const ndToken = await NDToken.deploy("Bagi Ngepet", "NGEPET");
+
   await greeter.deployed();
   await token.deployed();
+  await ndToken.deployed();
 
   console.log("Greeter deployed to:", greeter.address);
   console.log("Token deployed to:", token.address);
+  console.log("NDToken deployed to:", ndToken.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
